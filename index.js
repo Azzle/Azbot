21b921b2b859313931b96b680324824de6c10600
Azzle <abucher1@hotmail.com>
2018-07-13 18:06:29 -0500

    Added config file, start info, and comments

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
+// load up discord and commando library
 const Commando = require('discord.js-commando');
-const bot = new Commando.Client();
-const TOKEN = 'ENTERTOKENHERE'
 
+// names the bot
+const bot = new Commando.Client();
+
+// loads json file with token and prefix value
+const config = require("./config.json");
+
+// old token method
+// const TOKEN = 'test token'
+
+// commands list
 bot.registry.registerGroup('simple', 'Simple');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + '/commands');
 
+
+// Test Hello command
 bot.on('message', function(message) {
     if(message.content == 'Hello')
     {
@@ -13,7 +24,12 @@ bot.on('message', function(message) {
     }
 });
 
+
+// This will run if the bot runs and logs in successfully
 bot.on('ready',function(){
-    console.log("Ready");
-})
-bot.login(TOKEN);
+    console.log(`Bot has started, with ${bot.users.size} users, in ${bot.channels.size} channels of ${bot.guilds.size} guilds.`);
+    // Set bot status to number of served discord servers
+    // bot.user.setActivity(`Serving ${bot.guilds.size} servers`);
+});
+
+bot.login(config.token);
